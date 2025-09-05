401b4db660e8e339789b2b9cea239ffd40b50f8e
SwoopX <swoop0711@gmx.de>
2025-09-05 22:10:22 +0200

    Enable writing cstring attribute values

--- src/zm_attribute_info.cpp
+++ src/zm_attribute_info.cpp
@@ -262,6 +262,7 @@ void zmAttributeInfo::write()
     case deCONZ::Zcl128BitSecurityKey:
     case deCONZ::ZclSingleFloat:
     case deCONZ::ZclOctedString:
+    case deCONZ::ZclCharacterString:
         ok = getNumericInput();
         break;
 
@@ -561,6 +562,7 @@ void zmAttributeInfo::setAttribute(quint8 endpoint, quint16 clusterId, deCONZ::Z
     case deCONZ::Zcl128BitSecurityKey:
     case deCONZ::ZclSingleFloat:
     case deCONZ::ZclOctedString:
+    case deCONZ::ZclCharacterString:
         buildNumericInput();
         break;
 
@@ -627,6 +629,7 @@ void zmAttributeInfo::updateEdit()
     case deCONZ::Zcl128BitSecurityKey:
     case deCONZ::ZclSingleFloat:
     case deCONZ::ZclOctedString:
+    case deCONZ::ZclCharacterString:
         setNumericInput();
         break;
 
@@ -851,6 +854,18 @@ bool zmAttributeInfo::getNumericInput()
                 return true;
             }
             return false;
+        }
+        else if (m_attribute.dataType() == deCONZ::ZclCharacterString)
+        {
+            const QByteArray str = edit->text().toLatin1();
+
+            if (str.isEmpty())
+            {
+                return false;
+            }
+
+            m_attribute.setValue(QVariant(str));
+            return true;
         }
 
         if (m_signed)
